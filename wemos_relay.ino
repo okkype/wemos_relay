74070a0fccc20a472f2f967a0a6de6d02b3b1a04
Okky Permana <okkype@gmail.com>
2018-03-16 09:09:35 +0700

    re-formating code
    Signed-off-by: Okky Permana <okkype@gmail.com>

--- wemos_relay.ino
+++ wemos_relay.ino
@@ -3,28 +3,22 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char* ssid = "SIHIPO"; /* wifi ssid */
+const char* ssid = "Z2"; /* wifi ssid */
 const char* password = "sistemhidroponik"; /* wifi psk */
 const char* ap_ssid = "SIHIPO_AP"; /* AP ssid */
 const char* ap_password = "sistemhidroponik"; /* AP psk */
 const String device_id = "C01";
 const String device_type = "SIHIPO_C";
-// const boolean isAP = true;
 
 ESP8266WebServer server(80);
 
-boolean pin[9];
-int pinMap[] = {D1, D2, D3, D4, D8, D7, D6, D5}; /* Physical PIN Mapping */
+boolean pin[8];
+int pinMap[] = {D1, D2, D3, D4, D5, D6, D7, D8}; /* Physical PIN Mapping */
 
 void handleRoot() {
   String out = "{\"id\":\"" + device_id + "\",\"type\":\"" + device_type + "\",\"value\":[";
-  for(int c = 0; c < sizeof(pin); c++) {
+  for (int c = 0; c < sizeof(pin); c++) {
     int stat = pin[c] ? 1 : 0;
-    /* String out2 = "\"p";
-    out2 += c;
-    out2 += "\":";
-    out2 += stat;
-    out2 += " "; */
     String out2 = "";
     out2 += stat;
     out2 += " ";
@@ -92,35 +86,35 @@ void handlePin(int a) {
   handlePin(a, !pin[a]);
 }
 
-void handleNotFound(){
+void handleNotFound() {
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
   message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
   message += "\nArguments: ";
   message += server.args();
   message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
+  for (uint8_t i = 0; i < server.args(); i++) {
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
 }
 
-void setup(void){
+void setup(void) {
   pinMode(D0, INPUT);
-  for(int c = 0; c < sizeof(pin); c++) {
+  for (int c = 0; c < sizeof(pin); c++) {
     pinMode(pinMap[c], OUTPUT);
     pin[c] = false;
     handlePin(c, false);
   }
-  
-  if (digitalRead(D0) == HIGH) {
+
+  if (digitalRead(D0) == LOW) {
     Serial.begin(115200);
     Serial.println();
     Serial.print("Configuring access point...");
     WiFi.softAP(ap_ssid, ap_password);
-  
+
     IPAddress myIP = WiFi.softAPIP();
     Serial.print("AP IP address: ");
     Serial.println(myIP);
@@ -143,8 +137,8 @@ void setup(void){
     Serial.println("MDNS responder started");
   }
 
-  server.on("/", [](){
-    for (uint8_t i=0; i<server.args(); i++){
+  server.on("/", []() {
+    for (uint8_t i = 0; i < server.args(); i++) {
       if (server.argName(i).equals("p")) {
         boolean ar[sizeof(pin)];
         for (uint16_t i2 = 0; i2 < sizeof(pin); i2++) {
@@ -158,13 +152,13 @@ void setup(void){
 
   server.on("/ui", handleUI);
 
-  server.on("/0", [](){
-    if (server.args()==0) {
-      for(int c = 0; c < sizeof(pin); c++) {
+  server.on("/0", []() {
+    if (server.args() == 0) {
+      for (int c = 0; c < sizeof(pin); c++) {
         handlePin(c, false);
       }
     }
-    for (uint8_t i=0; i<server.args(); i++){
+    for (uint8_t i = 0; i < server.args(); i++) {
       if (server.argName(i).equals("p")) {
         handlePin(server.arg(i).toInt(), false);
       }
@@ -172,13 +166,13 @@ void setup(void){
     handleRoot();
   });
 
-  server.on("/1", [](){
-    if (server.args()==0) {
-      for(int c = 0; c < sizeof(pin); c++) {
+  server.on("/1", []() {
+    if (server.args() == 0) {
+      for (int c = 0; c < sizeof(pin); c++) {
         handlePin(c, true);
       }
     }
-    for (uint8_t i=0; i<server.args(); i++){
+    for (uint8_t i = 0; i < server.args(); i++) {
       if (server.argName(i).equals("p")) {
         handlePin(server.arg(i).toInt(), true);
       }
@@ -186,13 +180,13 @@ void setup(void){
     handleRoot();
   });
 
-  server.on("/2", [](){
-    if (server.args()==0) {
-      for(int c = 0; c < sizeof(pin); c++) {
+  server.on("/2", []() {
+    if (server.args() == 0) {
+      for (int c = 0; c < sizeof(pin); c++) {
         handlePin(c);
       }
     }
-    for (uint8_t i=0; i<server.args(); i++){
+    for (uint8_t i = 0; i < server.args(); i++) {
       if (server.argName(i).equals("p")) {
         handlePin(server.arg(i).toInt());
       }
@@ -206,6 +200,6 @@ void setup(void){
   Serial.println("HTTP server started");
 }
 
-void loop(void){
+void loop(void) {
   server.handleClient();
 }
